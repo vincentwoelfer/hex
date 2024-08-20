370ee64fc24eca39d404960df10cd5366232c13d
Vincent Wölfer <vincent.woelfer@gmail.com>
2024-08-20 10:50:30 +0200

    Restructure scenes

--- src/GenerateMap.gd
+++ src/GenerateMap.gd
@@ -1,21 +1,32 @@
-#@tool
+@tool
 extends Node3D
+
+var hex_geometry := preload("res://scenes/HexGeometry.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in range(-4, 4):
-		var mesh_generator_scene := preload("res://scenes/HexGeometry.tscn")
-		var tile : Node3D = mesh_generator_scene.instantiate()
-		
-		# Set parameters
-		#tile.width = 2.0
-		#tile.height = 2.0		
-		
-		tile.position = Vector3(i, 0, 0)
+	for x in range(0, 4):
+		for z in range(0, 4):
+			var isEven: bool = z % 2 == 0
+			var height: int = 0
+			var pos := Vector3.ZERO
+			pos.x = (x * HexConst.horizontal_size() * 2.0) + (HexConst.horizontal_size() / 2.0 if !isEven else 0.0)
+			pos.z = z * HexConst.vertical_size() * 1.0
+			pos.y = height
 
-		# Add to the current scene
-		add_child(tile)
-		tile.owner = self
+			# Instantiate
+			var tile: HexGeometry = hex_geometry.instantiate()
+		
+			var adjacent_hex: Array[HexGeometry.AdjacentHex] = [HexGeometry.AdjacentHex.new(0, ""), HexGeometry.AdjacentHex.new(0, ""), HexGeometry.AdjacentHex.new(1, ""), HexGeometry.AdjacentHex.new(2, ""), HexGeometry.AdjacentHex.new(0, ""), HexGeometry.AdjacentHex.new(-1, "")]
+			
+			# Set parameters
+			tile.height = height
+			tile.adjacent_hex = adjacent_hex
+			tile.position = pos
+
+			# Add to the current scene
+			add_child(tile)
+			#tile.owner = self
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
