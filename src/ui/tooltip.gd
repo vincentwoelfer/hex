5b80c9e0e61ff6fbec2ad82c472fe8884bd87024
ch4perone <ch4perone@gmail.com>
2024-09-20 17:02:12 +0200

    Add icons to Tooltip text

--- src/ui/tooltip.gd
+++ src/ui/tooltip.gd
@@ -5,6 +5,8 @@ extends PanelContainer
 @onready var header := %HeaderLabel
 @onready var tooltip := %TooltipLabel
 
+var icon_color: Color = Color.WHITE_SMOKE
+
 var tween: Tween
 
 func _ready() -> void:
@@ -25,10 +27,22 @@ func show_tooltip(hex_tile: HexTile) -> void:
 		tween.kill()
 	
 	header.text = "Meadow"
-	tooltip.text = "[ICON] " + str(snappedf(hex_tile.humidity, 0.1)) + "\t(Soil humidity)\n"
-	tooltip.text += "[ICON] " +  str(snappedf(1 - hex_tile.shade, 0.1)) + "\t(Sun exposure)\n"
-	tooltip.text += "[ICON] " +  str(snappedf(hex_tile.nutrition, 0.1)) + "\t(Nutrition)"
+	tooltip.text = ""
+	#color_humidity.a = clampf(1, 0.8, 1)
+	#label.push_color(color_humidity)
+	#label.push_outline_color(Color(1, 1, 1, 1 * label_scale))
+	tooltip.append_text(' [img color=#' + icon_color.to_html() + ']res://assets/icons/raindrop.png[/img]')
+	tooltip.append_text(str(snappedf(hex_tile.humidity, 0.1), " (Soil humidity)", '\n'))
 
+	# Shade
+	tooltip.append_text(' [img color=#' + icon_color.to_html() + ']res://assets/icons/shade_white.png[/img]')
+	tooltip.append_text(str(snappedf(1.0 - hex_tile.shade, 0.1), " (Sun exposure)",'\n'))
+
+	tooltip.append_text(' [img color=#' + icon_color.to_html() + ']res://assets/icons/nutrition.png[/img]')
+	tooltip.append_text(str(snappedf(hex_tile.shade, 0.1), " (Nutrition)", '\n'))
+
+	tooltip.pop_all()
+	
 	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_callback(show)
 	tween.tween_property(self, "modulate", Color.WHITE, fade_seconds)
