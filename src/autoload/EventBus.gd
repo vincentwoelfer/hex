0c2d8f045d912a717de6f335ecb3436733510f0c
ch4perone <ch4perone@gmail.com>
2024-09-26 19:36:28 +0200

    Add quit game keybinding

--- src/autoload/EventBus.gd
+++ src/autoload/EventBus.gd
@@ -49,8 +49,12 @@ func _input(event: InputEvent) -> void:
 			
 		if event.is_action_pressed("hold_speed_up_time"):
 			Signal_ToggleSpeedUpTime.emit()
+			
 		if event.is_action_released("hold_speed_up_time"):
 			Signal_ToggleSpeedUpTime.emit()
+			
+		if event.is_action_pressed("quit_game"):
+			get_tree().quit()
 			
 # Function to handle the signal
 func _on_Signal_HexConstChanged() -> void:
