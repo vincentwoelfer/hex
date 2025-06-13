09f8fff3c39e5276568fbdb0bb3052f170f08c20
Vincent Wölfer <vincent.woelfer@gmail.com>
2025-06-13 12:47:41 +0200

    fix

--- src/effects/AoeRangeIndicator.gd
+++ src/effects/AoeRangeIndicator.gd
@@ -3,11 +3,20 @@ class_name AoeRangeIndicator
 
 static var scene: PackedScene = preload("res://scenes/effects/AoeRangeIndicator.tscn")
 
-static func spawn(pos: Vector3, radius: float, lifetime: float) -> AoeRangeIndicator:
+static func spawn_global_pos(pos: Vector3, radius: float, lifetime: float) -> AoeRangeIndicator:
 	var instance := scene.instantiate() as AoeRangeIndicator
 	instance._resize_decal(radius)
 	Util.spawn(instance, pos, null)
 	Util.delete_after(lifetime, instance)
+	return instance
+
+
+static func spawn_at_parent(parent: Node3D, radius: float, lifetime: float) -> AoeRangeIndicator:
+	var instance := scene.instantiate() as AoeRangeIndicator
+	instance._resize_decal(radius)
+	Util.spawn(instance, Vector3.ZERO, parent)
+	Util.delete_after(lifetime, instance)
+	instance.top_level = true
 	return instance
 
 
